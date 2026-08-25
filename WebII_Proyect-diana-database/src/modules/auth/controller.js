dd75dfa77dd5a49216fe94c8426599aa1437edfe
Justin Chavarria Lopez <Justinchavarria8592@gmail.com>
2026-08-25 08:33:37 -0600

    Delete WebII_Proyect-diana-database directory

--- WebII_Proyect-diana-database/src/modules/auth/controller.js
+++
@@ -1,8 +0,0 @@
-const service = require('./service');
-
-const login = async (req, res) => {
-  const resultado = await service.login(req.body);
-  res.json({ success: true, ...resultado });
-};
-
-module.exports = { login };
--- WebII_Proyect-diana-database/src/utils/wrap.js
+++
@@ -1,5 +0,0 @@
-const wrap = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch(next);
-};
-
-module.exports = wrap;
